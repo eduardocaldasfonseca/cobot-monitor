315f0d78d37fdd3c3bb219132c97912cd961d245
eduardocaldasfonseca <eduardocaldasfonseca@gmail.com>
2021-06-10 06:56:43 +0100

    added/updated scripts that run openpose and openpifpaf

--- run_openpifpaf.py
+++ run_openpifpaf.py
@@ -1,23 +1,61 @@
-import io
-import numpy as np
-import PIL
+from PIL import Image
+import glob
 import openpifpaf
-import requests
-import torch
-import IPython
+import json
+from timeit import default_timer as timer
 
-print('OpenPifPaf version', openpifpaf.__version__)
-print('PyTorch version', torch.__version__)
+# === Run Variables (change these accordingly): === #
+run_number = 17
+model = 'shufflenetv2k16'  # pre-trained models available: resnet50, shufflenetv2k16 (chosen) and shufflenetv2k30
+# (performance metrics are available in the introduction page of the OpenPifPaf guide)
+dataset = 'left-small'
+create_images = False
+controlled_setup = True
 
-#
+# Additional path variables
+dataset_path = '/home/eduardocaldasfonseca/Desktop/cobot-monitor-dataset/' + dataset
+file_extension = '/*.jpg'  # type of file (change accordingly)
+output_path = '/home/eduardocaldasfonseca/Desktop/cobot-monitor-dataset/openpifpaf_output/run' + str(run_number)
 
-image_response = requests.get('https://raw.githubusercontent.com/openpifpaf/openpifpaf/main/docs/coco/000000081988.jpg')
-pil_im = PIL.Image.open(io.BytesIO(image_response.content)).convert('RGB')
-im = np.asarray(pil_im)
+# dictionary to build meta json that contains execution times and other information about the run
+meta = {'Run Number': run_number,
+        'OpenPifPaf version': str(openpifpaf.__version__),
+        'Model': model,
+        'Dataset': dataset,
+        'With image creation?': create_images,
+        'Controlled setup?': controlled_setup}
 
-predictor = openpifpaf.Predictor(checkpoint='shufflenetv2k16')
-predictions, gt_anns, image_meta = predictor.pil_image(pil_im)
+# === Create list containing all images of dataset: === #
+image_list = []  # empty list for all the images in dataset_path
+for file in glob.glob(dataset_path + file_extension):
+    im = Image.open(file)
+    image_list.append(im)
 
-annotation_painter = openpifpaf.show.AnnotationPainter()
-with openpifpaf.show.image_canvas(im) as ax:
-    annotation_painter.annotations(ax, predictions)
+# === Initialize OpenPifPaf Predictor and timers === #
+predictor = openpifpaf.Predictor(checkpoint=model)#, json_data=not create_images)  # the flag json_data changes the
+# predictions list to be json data instead of the annotations object (preventing the use of the annotation painter)
+start_total = timer()  # start total timer (includes additional operations so it's not very accurate)
+times = {}
+
+for image in image_list:
+    output_filename = image.filename.replace(dataset_path, '')
+    start = timer()  # start timer for this image
+    predictions, gt_anns, image_meta = predictor.pil_image(image)
+    end = timer()  # end timer for this image
+    times[output_filename] = str(end - start)  # saves execution time for this image in meta json
+
+    with open(output_path + output_filename + '_pifpaf_run' + str(run_number) + '_keypoints.json', 'w',
+              encoding='utf-8') as f:
+        json.dump([ann.json_data() for ann in predictions], f, ensure_ascii=False, indent=4)
+
+    if create_images:  # outputs images with skeleton overlayed
+        annotation_painter = openpifpaf.show.AnnotationPainter()
+        with openpifpaf.show.image_canvas(image, output_path + output_filename + '_pifpaf_run' + str(run_number) + '_rendered.jpg') as ax:
+            annotation_painter.annotations(ax, predictions)
+
+
+end_total = timer()  # end total timer
+meta['Times'] = times
+meta['Total time'] = str(end_total - start_total)
+with open(output_path + '/pifpaf_run' + str(run_number) + '_meta.json', 'w', encoding='utf-8') as f:
+    json.dump(meta, f, ensure_ascii=False, indent=4)
--- run_openpose.py
+++ run_openpose.py
@@ -1,6 +1,7 @@
 import sys
 import cv2
 import argparse
+import json
 from timeit import default_timer as timer
 
 # === Import Openpose === #
@@ -11,34 +12,56 @@ except ImportError as e:
     print('Error: OpenPose library could not be found. Check if path is correct.')
     raise e
 
+# === Run Variables (change these accordingly): === #
+run_number = 14
+model = 'BODY_25'  # pre-trained models available: 'BODY_25' (fastest, most accurate and includes foot keypoints),
+# `COCO` (18 keypoints), `MPI` (15 keypoints, least accurate model but fastest on CPU)
+dataset = 'right-small'
+create_images = False
+controlled_setup = True
+max_n_people = -1
+net_resolution = '-1x368'  # Default number -1x368
+
+# Additional path variables
+dataset_path = '/home/eduardocaldasfonseca/Desktop/cobot-monitor-dataset/' + dataset
+file_extension = 'jpg'  # type of file (change accordingly)
+output_path = '/home/eduardocaldasfonseca/Desktop/cobot-monitor-dataset/openpose_output/run' + str(run_number)
+
+# dictionary to build meta json that contains execution times and other information about the run
+meta = {'Run Number': run_number,
+        'OpenPose version': 'check individual json files',
+        'Model': model,
+        'Dataset': dataset,
+        'With image creation?': create_images,
+        'Controlled setup?': controlled_setup,
+        'Number people max flag': max_n_people,
+        'Net resolution flag': net_resolution}
+
 try:
     # === Flags === #
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image_dir", default="/home/eduardocaldasfonseca/Desktop/cobot-monitor-dataset/test")  #
-    # change this path accordingly
+    parser.add_argument("--image_dir", default=dataset_path)
     parser.add_argument("--no_display", default=False, help="Enable to disable the visual display.")
     args = parser.parse_known_args()
 
     # === Custom Parameters (refer to /include/openpose/flags.hpp for more parameters) === #
     params = dict()
     params["model_folder"] = "/home/eduardocaldasfonseca/openpose/models/"
-    params["number_people_max"] = -1  # This parameter will limit the maximum number of people detected, by keeping
-    # the people with the top scores (based on person area over the image, body part score, and joint score.
+    params["number_people_max"] = max_n_people  # This parameter will limit the maximum number of people detected,
+    # by keeping the people with the top scores (based on person area over the image, body part score, and joint score.
     # The default -1 keeps them all.
     params["fps_max"] = -1  # Maximum processing frame rate. By default (-1), will process frames as fast as possible.
-    params["model_pose"] = "BODY_25"  # Model to be used: 'BODY_25' (fastest, most accurate and includes foot
-    # keypoints), `COCO` (18 keypoints), `MPI` (15 keypoints, least accurate model but fastest on CPU)
-    params["net_resolution"] = "-1x368"  # Default. Increasing will potentially increase accuracy. Decreasing will
-    # increase speed
+    params["model_pose"] = model  # Model to be used
+    params["net_resolution"] = net_resolution  # Increasing may increase accuracy. Decreasing will increase speed
     # params["display"] = 0  # No display since visual output is not required. (-1 is default for automatic selection)
 
     # === Output Parameters === #
-    params["write_images"] = "/home/eduardocaldasfonseca/Desktop/cobot-monitor-dataset/openpose_output"  # Directory
-    # to write rendered frames
-    params["write_images_format"] = "jpg"
-    params["write_json"] = "/home/eduardocaldasfonseca/Desktop/cobot-monitor-dataset/openpose_output"  # .json output
+    if create_images:
+        params["write_images"] = output_path  # Directory to write rendered frames
+        params["write_images_format"] = file_extension
+    params["write_json"] = output_path  # .json output
 
-    # === Add other images from directory === #
+    # === Add images from directory === #
     for i in range(0, len(args[1])):
         curr_item = args[1][i]
         if i != len(args[1]) - 1:
@@ -54,10 +77,6 @@ try:
             if key not in params:
                 params[key] = next_item
 
-    # Construct it from system arguments
-    # op.init_argv(args[1])
-    # oppython = op.OpenposePython()
-
     # Starting OpenPose
     opWrapper = op.WrapperPython()
     opWrapper.configure(params)
@@ -65,6 +84,7 @@ try:
 
     # Read frames on directory
     imagePaths = op.get_images_on_directory(args[0].image_dir)
+    times = {}  # dictionary to build execution times json
     start_total = timer()
 
     # Process and display images
@@ -77,17 +97,16 @@ try:
         start = timer()
         opWrapper.emplaceAndPop(op.VectorDatum([datum]))
         end = timer()
-        print("Total time: " + str(end - start) + " seconds")
-
+        times[str(datum.name)] = str(end - start)
+        # print("Total time: " + str(end - start) + " seconds")
         # print("Body keypoints: \n" + str(datum.poseKeypoints))
 
-        # if not args[0].no_display:
-        #     cv2.imshow("OpenPose 1.7.0 - Python API Test", datum.cvOutputData)
-        #     key = cv2.waitKey(15)
-        #     if key == 27: break
-
+    # print("OpenPose demo successfully finished. Total time: " + str(end_total - start_total) + " seconds")
     end_total = timer()
-    print("OpenPose demo successfully finished. Total time: " + str(end_total - start_total) + " seconds")
+    meta['Times'] = times
+    meta['Total time'] = str(end_total - start_total)
+    with open(output_path + '/openpose_run' + str(run_number) + '_meta.json', 'w', encoding='utf-8') as f:
+        json.dump(meta, f, ensure_ascii=False, indent=4)
 
 except Exception as e:
     print(e)
